f6858c736eda9090bb7cd3785eb1d7d6022a77bb
Adrian Bowyer <adrian@reprappro.com>
2019-01-30 11:52:12 +0000

    File reorganisation.

---
+++ Legacy/Software/Debug/src/Painter.d
@@ -0,0 +1,8 @@
+src/Painter.d: ../src/Painter.cpp ../src/Painter.h \
+ /usr/include/opencv/cv.h /usr/include/opencv/highgui.h
+
+../src/Painter.h:
+
+/usr/include/opencv/cv.h:
+
+/usr/include/opencv/highgui.h:
