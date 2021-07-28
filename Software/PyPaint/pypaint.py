c9fcb5e59656650e14686a5621c47f2824898f24
Adrian Bowyer <adrian@reprappro.com>
2021-07-28 12:42:06 +0100

    Added scan of the experimental palette. Colour pick now aveages over a small square.

--- Software/PyPaint/pypaint.py
+++ Software/PyPaint/pypaint.py
@@ -20,22 +20,23 @@ import serial
 debug = True
 
 class Mixer:
- def __init__(self):
-  self.usb = serial.Serial('/dev/ttyACM0',115200,timeout=0.1)
+ def __init__(self, live):
+  if live:
+   self.usb = serial.Serial('/dev/ttyACM0',115200,timeout=0.1)
+   # Tell it to use relative moves
+   self.usb.write(str.encode("G91\n"))
   # Assume all retracted from last use
   self.retracted = [True]*4
   # Millilitres per unit of movement (i.e. reciprocal of what volume you get for G1 X1)
   self.milliL = 1.0
   # Extrusion rate, ml/second
-  self.extrudeRate = 0.05
+  self.extrudeRate = 0.1
   # Retraction rate, ml/second
-  self.retractionRate = 0.1
-  # How small a movement can we consider to be zero?
-  self.zeroMovement = self.milliL*0.0001
+  self.retractionRate = 0.3
   # How many ml to retract
   self.retractMl = 0.1
-  # Tell it to use relative moves
-  self.usb.write(str.encode("G91\n"))
+  self.live = live
+
 
  def Move(self, movements, f):
   s = "G1 X"
@@ -49,7 +50,8 @@ class Mixer:
   s += " F"
   s += str(f)
   s += "\n"
-  self.usb.write(str.encode(s))
+  if self.live:
+   self.usb.write(str.encode(s))
   if debug:
    print("Sent: ", s)
 
@@ -60,12 +62,8 @@ class Mixer:
   movements = [0.0]*4
   for v in range(4):
    m = volumes[v]*self.milliL
-   if abs(m) < self.zeroMovement:
-    m = 0.0
-    self.retracted[v] = True
-   else:
-    if self.retracted[v]:
-     m = m + self.retractMl*self.milliL
+   if self.retracted[v]:
+    m = m + self.retractMl*self.milliL
     self.retracted[v] = False
    movements[v] = m
   self.Move(movements, self.Feed(self.extrudeRate))
@@ -114,17 +112,35 @@ def ColourScale(colour):
  b = colour[2]/255.0
  return (r, g, b)
 
-
+def FloatsToString(floats):
+ s = " "
+ for f in floats:
+  s += '%.4f' % f
+  s += " "
+ s = "(" + s + ")"
+ return s
 
 class Picture:
 
+ # This does an avearage in the 5x5 square round the pixel
+ # This may not be what we always want...
  def callback(self, event):
-  pColour = self.pixels[event.x,event.y]
+  colour = [0.0, 0.0, 0.0]
+  for x in range(event.x - 2, event.x + 3):
+   for y in range(event.y - 2, event.y + 3):
+    pColour = self.pixels[x,y]
+    c = ColourScale(pColour)
+    colour[0] += c[0]
+    colour[1] += c[1]
+    colour[2] += c[2]
+  colour[0] /= 25.0
+  colour[1] /= 25.0
+  colour[2] /= 25.0
+  pColour = (int(round(colour[0]*255.0)), int(round(colour[1]*255.0)), int(round(colour[2]*255.0)))
   self.selectedColour.configure(bg='#%02x%02x%02x' % pColour)
-  c = ColourScale(pColour)
-  self.cmyw = RGBtoCMYW(c)
+  self.cmyw = RGBtoCMYW(colour)
   if debug:
-   print("x = ", event.x, ", y = ", event.y, ", rgb: ", c, ", cmyw: ", self.cmyw)
+   print("x =", event.x, ", y =", event.y, ", rgb:", FloatsToString(colour), ", cmyw:", FloatsToString(self.cmyw))
 
 
  def Retract(self):
@@ -151,38 +167,61 @@ class Picture:
  def Extrude1(self):
   self.Extrude(1.0)
 
- def __init__(self, name):
+ def Quit(self):
+  self.Retract()
+  quit()
+
+ def __init__(self, name, live):
   self.window = tkinter.Tk(className=name)
-  self.mixer = Mixer()
+  self.mixer = Mixer(live)
   self.image = Image.open(name)
   self.pixels = self.image.load()
-  self.canvas = tkinter.Canvas(self.window, width=self.image.size[0], height=self.image.size[1])
+  self.canvas = tkinter.Canvas(self.window, width=self.image.size[0]+150, height=self.image.size[1])
   self.canvas.pack()
   image_tk = ImageTk.PhotoImage(self.image)
   self.canvas.create_image(self.image.size[0]//2, self.image.size[1]//2, image=image_tk)
 
   self.selectedColour = tkinter.Button(text="", width=10, height=3, bg="white", fg="white")
   self.selectedColour.pack()
+  yPos = 10
+  self.selectedColour.place(x=self.image.size[0]+20, y = yPos)
   self.retract = tkinter.Button(text="retract", width=10, height=3, bg="grey", fg="white",command=self.Retract)
   self.retract.pack()
+  yPos += 70
+  self.retract.place(x=self.image.size[0]+20, y = yPos)
 
   self.e005 = tkinter.Button(text="0.05 ml", width=10, height=3, bg="grey", fg="white",command=self.Extrude005)
   self.e005.pack()
+  yPos += 70
+  self.e005.place(x=self.image.size[0] + 20, y=yPos)
   self.e01 = tkinter.Button(text="0.1 ml", width=10, height=3, bg="grey", fg="white",command=self.Extrude01)
   self.e01.pack()
+  yPos += 70
+  self.e01.place(x=self.image.size[0] + 20, y=yPos)
   self.e02 = tkinter.Button(text="0.2 ml", width=10, height=3, bg="grey", fg="white",command=self.Extrude02)
   self.e02.pack()
+  yPos += 70
+  self.e02.place(x=self.image.size[0] + 20, y=yPos)
   self.e05 = tkinter.Button(text="0.5 ml", width=10, height=3, bg="grey", fg="white",command=self.Extrude05)
   self.e05.pack()
+  yPos += 70
+  self.e05.place(x=self.image.size[0] + 20, y=yPos)
   self.e1 = tkinter.Button(text="1 ml", width=10, height=3, bg="grey", fg="white",command=self.Extrude1)
   self.e1.pack()
+  yPos += 70
+  self.e1.place(x=self.image.size[0] + 20, y=yPos)
+
+  self.quit = tkinter.Button(text="Quit", width=10, height=3, bg="grey", fg="white",command=self.Quit)
+  self.quit.pack()
+  yPos += 100
+  self.quit.place(x=self.image.size[0] + 20, y=yPos)
 
   self.canvas.bind("<Button-1>", self.callback)
   self.cmyw = RGBtoCMYW((1, 1, 1))
   self.window.mainloop()
 
 #p = Picture('../../Artworks/ai6-nc.jpg')
-p = Picture('../../Artworks/palette.jpg')
+p = Picture('../../Artworks/combined-palette.jpg', False)
 
 
 
