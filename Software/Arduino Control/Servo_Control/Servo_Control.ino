bc6bbfad4fd51a68d000a15a6118b5ef80b60960
Adrian Bowyer <adrian@reprappro.com>
2024-05-29 14:58:45 +0100

    Updating

--- Software/Arduino Control/Servo_Control/Servo_Control.ino
+++ Software/Arduino Control/Servo_Control/Servo_Control.ino
@@ -1,30 +1,53 @@
 //130 degress = off. Directly infront of lens
 //170 degrees = lens exposed.
 
-const int OpenAngle = 170;
-const int ClosedAngle = 130;
+const int OpenAngle = 60;
+const int ClosedAngle = 150;
 // Include the Servo library 
 #include <Servo.h> 
 // Declare the Servo pin 
 int servoPin = 3; 
+int buttonPin=4;
 // Create a servo object 
-Servo Servo1; 
+Servo Servo1;
+
 void setup() 
-
 { // We need to attach the servo to the used pin number 
-Servo1.attach(servoPin); 
-Serial.begin(9600);
-Serial.println("Servo Control");
-Servo1.write(OpenAngle);}
+  Servo1.attach(servoPin); 
+  Serial.begin(9600);
+  Serial.println("Servo Control");
+  Servo1.write(OpenAngle);
+  pinMode(buttonPin,INPUT_PULLUP);
+}
 
-
-void loop(){ if (Serial.available()>0)
-{int open =Serial.parseInt();
-Servo1.write(open);
-    //if (open) 
-  // {// Make servo go to angle typed
-  // Servo1.write(OpenAngle); } else
-  //{Servo1.write(ClosedAngle);}
-   while (Serial.available())
-   Serial.read();}
+int buttonopen=1;
+void loop()
+{ 
+  if (Serial.available()>0)
+  {  
+    int open = Serial.parseInt();
+    //Servo1.write(open);
+    if (open) 
+    {// Make servo go to angle typed
+      Servo1.write(OpenAngle);
+    } else
+    {
+      Servo1.write(ClosedAngle);
+    }
+    while (Serial.available()) Serial.read();
+  }
+  if (!digitalRead(buttonPin))
+  {
+    delay(10);
+     buttonopen=1-buttonopen; 
+    if (buttonopen) 
+    {// Make servo go to angle typed
+      Servo1.write(OpenAngle);
+    } else
+    {
+      Servo1.write(ClosedAngle);
+    }
+    delay(1000);
+  }
+  
 }
